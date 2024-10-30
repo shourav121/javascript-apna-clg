48e97f991f4d70724bae0a9b2e459124a1012c6b
shourav kumar <shouravk81@gmail.com>
2024-10-30 12:04:24 +0530

    work

---
+++ javascript/first.js
@@ -0,0 +1,44 @@
+// const student = {
+//     fullName :"shourav kuamr ",
+//     age : 44,
+//     cgpa : 9,
+//     state : "bihar"
+// }
+// console.log(student.age);
+// student.age=student.age+1
+// student.name="hghghghghghghg"
+
+// const profile= {
+//     username : "shourav kumar",
+//     isFillow : true,
+//     post : 195,
+//     followers : 258,
+//     following : 5,
+//     description : "nbhghchjhgyfmhj"
+
+// }
+// console.log(profile)
+
+
+                                                   //unary operator
+// let a=9;
+// let b=2;
+
+// console.log("a=",a,"b=",b)
+// a+=6;
+// console.log("a+6=",a)
+  
+                                           //comparision operator
+// let a=8;
+// let b="7";;
+// console.log(a>=b)
+                                               //logical operator
+let a=5;
+let b=7;
+let cond1 = a>b;//!false
+let cond2 = a>7;//!false
+console.log("cond1|| cond2=",cond1||cond2)//!output is false
+
+let f=8;
+letu=6;
+console.log("")
