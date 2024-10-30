5e408f0f8b3d09da3d18725b61acd2258f9c8805
shourav kumar <shouravk81@gmail.com>
2024-10-30 12:07:07 +0530

    work

---
+++ operator/alloperator.js
@@ -0,0 +1,21 @@
+
+                                                   //unary operator
+// let a=9;
+// let b=2;
+
+// console.log("a=",a,"b=",b)
+// a+=6;
+// console.log("a+6=",a)
+  
+                                           //comparision operator
+// let a=8;
+// let b="7";;
+// console.log(a>=b)
+                                               //logical operator
+                                               let a=5;
+                                               let b=7;
+                                               let cond1 = a>b;//!false
+                                               let cond2 = a>7;//!false
+                                               console.log("cond1|| cond2=",cond1||cond2)//!output is false
+                                               
+                                               
